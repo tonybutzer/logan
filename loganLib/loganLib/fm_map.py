b3247ba89abc58bf20fb26e1b4053544f442a424
tonybutzer <tonybutzer@gmail.com>
2020-12-14 12:52:09 +0000

    automatic git update from Makefile

--- loganLib/loganLib/fm_map.py
+++ loganLib/loganLib/fm_map.py
@@ -50,3 +50,10 @@ class Fmap():
                 style_function= lambda x:{'color':color}
                 folium.GeoJson(my_geo_json,style_function).add_to(map3)
                 return(map3)
+
+	def ne_geojson(self, geojson_bb_file):
+                my_geo = gpd.read_file(geojson_bb_file)
+                center=my_geo.geometry[0].bounds[1], my_geo.geometry[0].bounds[0]
+                sw = center
+                ne = my_geo.geometry[0].bounds[3], my_geo.geometry[0].bounds[2]
+                return(ne)
